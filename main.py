b0ec1f089225161637cbfc8c6e8c816bf5dffd33
Shubhanshu-G <subanshugupta135@gmail.com>
2026-08-11 18:35:56 +0530

    Model Training

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from networksecurity.entity.artifact_entity import (
     DataTransformationArtifact
 )
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 from networksecurity.components.data_transformation import DataTransformation
 
 import os
@@ -37,12 +39,12 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("Data Transformation completed")
 
-        # logging.info("Model Training sstared")
-        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
-        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
-        # model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
 
-        # logging.info("Model Training artifact created")
+        logging.info("Model Training artifact created")
         
         
         
